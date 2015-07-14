e7eef9f01df68cccaed10f000dd6050183628f89
Adriano Fernandes <adrianu_f22@hotmail.com>
2015-07-14 14:55:21 -0300

    Correção do caminho do módulo
    Correção do caminho do módulo - /module/nome do arquivo

--- module_builder/module.bat
+++ module_builder/module.bat
@@ -6,43 +6,43 @@ SET /P mname=Nome do modulo:
 mkdir admin
 
 mkdir admin\controller
-mkdir admin\controller\%mname%
-copy NUL admin\controller\%mname%\%mname%.php
+mkdir admin\controller\module\
+copy NUL admin\controller\module\%mname%.php
 
 mkdir admin\language
 mkdir admin\language\english
-mkdir admin\language\english\%mname%
-copy NUL admin\language\english\%mname%\%mname%.php
+mkdir admin\language\english\module
+copy NUL admin\language\english\module\%mname%.php
 
 mkdir admin\model
-mkdir admin\model\%mname%
-copy NUL admin\model\%mname%\%mname%.php
+mkdir admin\model\module
+copy NUL admin\model\module\%mname%.php
 
 mkdir admin\view
 mkdir admin\view\template
-mkdir admin\view\template\%mname%
-copy NUL admin\view\template\%mname%\%mname%.tpl
+mkdir admin\view\template\module
+copy NUL admin\view\template\module\%mname%.tpl
 
 mkdir catalog
 mkdir catalog\controller
-mkdir catalog\controller\%mname%
-copy NUL catalog\controller\%mname%\%mname%.php
+mkdir catalog\controller\module
+copy NUL catalog\controller\module\%mname%.php
 
 mkdir catalog\language
 mkdir catalog\language\english
-mkdir catalog\language\english\%mname%
-copy NUL catalog\language\english\%mname%\%mname%.php
+mkdir catalog\language\english\module
+copy NUL catalog\language\english\module\%mname%.php
 
 mkdir catalog\model
-mkdir catalog\model\%mname%
-copy NUL catalog\model\%mname%\%mname%.php
+mkdir catalog\model\module
+copy NUL catalog\model\module\%mname%.php
 
 mkdir catalog\view
 mkdir catalog\view\theme
 mkdir catalog\view\theme\default
 mkdir catalog\view\theme\default\template
-mkdir catalog\view\theme\default\template\%mname%
-copy NUL catalog\view\theme\default\template\%mname%\%mname%.tpl
+mkdir catalog\view\theme\default\template\module
+copy NUL catalog\view\theme\default\template\module\%mname%.tpl
 
 ECHO.
 ECHO Estrutura criada com sucesso!
